ef8ba2e65ece81508c428b5fbb0f144b8d2e512d
TUDONG05 <khactu731@gmail.com>
2024-11-11 16:42:53 +0700

    Add files via upload

---
+++ tinh_luong_gv/Test.java
@@ -0,0 +1,15 @@
+package tinh_luong_gv;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		GVThinhGiang t1 = new GVThinhGiang();
+		t1.nhapTT();
+		t1.inThongTin();
+		
+		
+//		GVCoHuu c1 = new GVCoHuu();
+//		c1.nhapTT();
+//		c1.inThongTin();
+	}
+}
