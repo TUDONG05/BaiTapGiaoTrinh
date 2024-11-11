31a2055f05d8a58b5529021b09c75e5a16c4d35e
TUDONG05 <khactu731@gmail.com>
2024-11-11 17:07:56 +0700

    Add files via upload

---
+++ BaiTapChuong3/tinh_diem_sv/Test.java
@@ -0,0 +1,13 @@
+package tinh_diem_sv;
+
+public class Test {
+public static void main(String[] args) {
+	SVCDN n1 = new SVCDN();
+	n1.nhapThongTin();
+	n1.inThongTin();
+	
+	SVCDCQ n2 = new SVCDCQ();
+	n2.nhapThongTin();
+	n2.inThongTin();
+}
+}
