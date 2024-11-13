b4b29cfae62568742e700127d4a306f19e47e402
TUDONG05 <khactu731@gmail.com>
2024-11-13 08:01:11 +0700

    Add files via upload

---
+++ BaiTapChuong3/tinh_luong_cong_nhan/Test.java
@@ -0,0 +1,15 @@
+package tinh_luong_cong_nhan;
+
+public class Test {
+	
+	public static void main(String[] args) {
+//		CNKSP c = new CNKSP();
+//		c.nhapTT();
+//		c.inTT();
+//		
+		
+		CNTCN n1 = new CNTCN();
+		n1.nhapTT();
+		n1.inTT();
+	}
+}
