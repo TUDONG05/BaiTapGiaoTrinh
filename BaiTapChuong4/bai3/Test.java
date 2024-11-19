e6ce754b345902f6f88e173558c6afcc93a8888f
TUDONG05 <khactu731@gmail.com>
2024-11-19 11:16:44 +0700

    Update Test.java

--- BaiTapChuong4/bai3/Test.java
+++ BaiTapChuong4/bai3/Test.java
@@ -1,38 +1,29 @@
 package bai3;
-import java.util.Scanner;
-public class Test {
-public static void main(String[] args) throws StringTooLongexception {
-	String ch[]= new String[100];
-	
-	Scanner input = new Scanner(System.in);
-	System.out.println("Moi nhap cac chuoi ki tu!!");
-	String d ="DONE";
-	
-	
-	try {
-		for(int i=0;i<ch.length;i++) {
-		
-		ch[i] = input.nextLine();
-		if(ch[i].equals(d))
-			break;
-	}	
-		try {
-			for(int i=0;i<ch.length;i++)
-	if(ch[i].length()>30) throw new StringTooLongexception("vuot qua so ki tu quy dinh!!");
-		}
-		catch( NullPointerException l) {
-			System.out.println(l.getMessage());
-		}
-	
-	}
-	catch (StringTooLongexception e) {
-		System.out.println("Loi: " +e.getMessage());
-	}
-	
-	
-	
 
-		
-		
-}
+import java.util.Scanner;
+
+public class Test {
+	public static void main(String[] args) throws StringTooLongexception {
+
+		Scanner input = new Scanner(System.in);
+		try {
+			System.out.println("Nhap cac chuoi, neu gap chuoi DONE thi ket thuc chuong trinh");
+			while (true) {
+				System.out.println("Nhap chuoi: ");
+				String a = input.nextLine();
+				if (a.equalsIgnoreCase("DONE")) {
+					System.out.println("Ket thuc chuong trinh!");
+					break;
+				}
+				if (a.length() > 30) {
+					System.out.println("Ket thuc chuong trinh!");
+					throw new StringTooLongexception("Loi nhap qua ki tu!");
+				}
+			}
+		}
+		catch (StringTooLongexception e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
 }
