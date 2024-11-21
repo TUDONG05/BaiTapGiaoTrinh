f556182ed9b64288f2868d52b40f1bb09b271864
TUDONG05 <khactu731@gmail.com>
2024-11-21 23:05:04 +0700

    Add files via upload

---
+++ BaiTapChuong3/cao_dang_x/InputException.java
@@ -0,0 +1,14 @@
+package cao_dang_x;
+
+public class InputException extends Exception{
+	protected String mess;
+	public InputException() {
+		
+	}
+	public InputException(String mess) {
+		super(mess);
+	}
+	
+	
+
+}
---
+++ BaiTapChuong3/cao_dang_x/Test.java
@@ -0,0 +1,92 @@
+package cao_dang_x;
+import java.util.Scanner;
+public class Test {
+	
+//	sap xep theo diem giam dan
+	public void nhap(SVCDCQ sv[]) {
+		//khoi tao gia tri ban dau va nhap thong tin
+		for(int i=0;i<sv.length;i++) {
+			sv[i]=new SVCDCQ();
+			sv[i].nhapThongTin();
+		}
+
+	}
+	public void sapGiam(SVCDCQ sv[]) {
+			for(int i=0;i<sv.length-1;i++)
+		for(int j=i+1;j<sv.length;j++) 
+			if(sv[i].tinhDiem()<sv[j].tinhDiem()) {
+				SVCDCQ tg= new SVCDCQ();
+				tg=sv[i];
+				sv[i]=sv[j];
+				sv[j]=tg;
+			
+			}
+	}
+	public void in(SVCDCQ sv[]) {
+
+		//in thong tin
+		for(int i=0;i<sv.length;i++)
+			sv[i].inThongTin();
+	}
+	
+//	check ton tai
+	public void checking(SVCDCQ sv[],String msv) {
+		boolean check=false;
+	SVCDCQ ch = new SVCDCQ();
+	
+	for(int i=0;i<sv.length;i++)
+		if(sv[i].getMsv().equals(msv)) {
+			check=true;
+			ch=sv[i];
+		}
+	
+	if(check)
+		ch.inThongTin();
+	else
+		System.out.println("ko ton tai msv do!");
+	}
+	
+			
+	public static void main(String[] args) {
+
+	
+		
+		int n;
+		Scanner input = new Scanner(System.in);
+		do {
+			System.out.println("Nhap so sinh vien can tao");
+			n =input.nextInt();
+			
+		}
+		while(n<2 || n>4);
+		
+		
+//			try {
+//				Nhap vao = new Nhap(n);
+//			}
+//			catch(InputException e) {
+//				System.out.println(e.getMessage());
+//			}
+		
+			
+			
+		SVCDCQ sv[] = new SVCDCQ[n];
+		
+		Test test = new Test();
+		test.nhap(sv);
+		test.in(sv);
+		
+		System.out.println("Nhap msv can tim:");
+		String msv = input.next();
+		test.checking(sv, msv);
+		
+		System.out.println("Danh sach sv diem giam dan");
+		test.sapGiam(sv);
+		test.in(sv);
+		
+		
+		
+	
+	
+}
+}
